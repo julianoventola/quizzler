39f813243a447ca4e7578641de5238f92dbd1979
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-11 02:43:38 -0300

    feat: quizzler layout page

--- lib/pages/quizzler_page.dart
+++ lib/pages/quizzler_page.dart
@@ -10,8 +10,76 @@ class QuizzlerPage extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Container(),
+        child: QuizPage(),
       )),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              'This is where the question text will go',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: TextButton(
+              style: ButtonStyle(
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(fontSize: 20)),
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.green,
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(
+                  Colors.white,
+                ),
+              ),
+              onPressed: () {},
+              child: Text('True'),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: TextButton(
+              style: ButtonStyle(
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(fontSize: 20)),
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.red,
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(
+                  Colors.white,
+                ),
+              ),
+              onPressed: () {},
+              child: Text('False'),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
