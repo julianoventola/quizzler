7e8a0ae54908e84c24b59a6a85051b1e875afcd9
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-13 02:21:25 -0300

    refact: moved quiz_page to another file

--- lib/pages/quizzler_page.dart
+++ lib/pages/quizzler_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/pages/quiz_page.dart';
 
 class QuizzlerPage extends StatelessWidget {
   const QuizzlerPage({Key? key}) : super(key: key);
@@ -12,87 +13,6 @@ class QuizzlerPage extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 10),
         child: QuizPage(),
       )),
-    );
-  }
-}
-
-class QuizPage extends StatefulWidget {
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          flex: 5,
-          child: Center(
-            child: Text(
-              'This is where the question text will go',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            child: TextButton(
-              style: ButtonStyle(
-                textStyle: MaterialStateProperty.all<TextStyle>(
-                    TextStyle(fontSize: 20)),
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.green,
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Colors.white,
-                ),
-              ),
-              onPressed: () {},
-              child: Text('True'),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            child: TextButton(
-              style: ButtonStyle(
-                textStyle: MaterialStateProperty.all<TextStyle>(
-                    TextStyle(fontSize: 20)),
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.red,
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Colors.white,
-                ),
-              ),
-              onPressed: () {},
-              child: Text('False'),
-            ),
-          ),
-        ),
-        // refact: improve score keeper
-        Row(
-          children: [
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          ],
-        )
-      ],
     );
   }
 }
